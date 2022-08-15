a2c55a6b0c0eb779ab349cddeadba7db4ef1488f
Super Liu <supercjliu@google.com>
2022-08-15 17:26:35 +0800

    touch/novatek: use GTI to request threaded irq.
    Bug: 201610482
    Signed-off-by: Super Liu <supercjliu@google.com>
    Change-Id: I4edf97ca954356947de9cf91ddbdc31790150c85

--- nt36xxx/nt36xxx.c
+++ nt36xxx/nt36xxx.c
@@ -2477,8 +2477,9 @@ static int32_t nvt_ts_probe(struct spi_device *client)
 	if (client->irq) {
 		NVT_LOG("int_trigger_type=%d\n", ts->int_trigger_type);
 		ts->irq_enabled = true;
-		ret = request_threaded_irq(client->irq, nvt_ts_isr, nvt_ts_work_func,
-					   ts->int_trigger_type | IRQF_ONESHOT, NVT_SPI_NAME, ts);
+		ret = goog_request_threaded_irq(ts->gti, client->irq,
+				nvt_ts_isr, nvt_ts_work_func,
+				ts->int_trigger_type | IRQF_ONESHOT, NVT_SPI_NAME, ts);
 		if (ret != 0) {
 			NVT_ERR("request irq failed. ret=%d\n", ret);
 			goto err_int_request_failed;
