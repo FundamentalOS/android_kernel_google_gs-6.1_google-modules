oberdfr/google-modules_wlan_bcm4389

bcmdhd: switch from 'pci_' to 'dma_' API
Upstream removed the 'pci_' APIs in commit 7968778914e5 ("PCI: Remove
the deprecated "pci-dma-compat.h"), which were trivial wrappers around
the 'dma_' equivalents, so use these instead.

Bug: 229037623
Signed-off-by: Carlos Llamas <cmllamas@google.com>
Change-Id: I2ef2583a07b7c822dbfc8bc8b55f70b0eabab1fe

dhd_pcie_linux.c
@@ -326,8 +326,8 @@ static int dhdpcie_smmu_init(struct pci_dev *pdev, void *smmu_cxt)
 
 	DHD_ERROR(("%s : SMMU init start\n", __FUNCTION__));
 
-	if (pci_set_dma_mask(pdev, DMA_BIT_MASK(64)) ||
-		pci_set_consistent_dma_mask(pdev, DMA_BIT_MASK(64))) {
+	if (dma_set_mask(&pdev->dev, DMA_BIT_MASK(64)) ||
+		dma_set_coherent_mask(&pdev->dev, DMA_BIT_MASK(64))) {
 		DHD_ERROR(("%s: DMA set 64bit mask failed.\n", __FUNCTION__));
 		return -EINVAL;
 	}
@@ -2088,8 +2088,8 @@ int dhdpcie_init(struct pci_dev *pdev)
 
 #ifdef DHD_SET_PCIE_DMA_MASK_FOR_GS101
 		/* S.SLSI PCIe DMA engine cannot support 64 bit bus address. Hence, set 36 bit */
-		if (pci_set_dma_mask(pdev, DMA_BIT_MASK(DHD_PCIE_DMA_MASK_FOR_GS101)) ||
-			pci_set_consistent_dma_mask(pdev,
+		if (dma_set_mask(&pdev->dev, DMA_BIT_MASK(DHD_PCIE_DMA_MASK_FOR_GS101)) ||
+			dma_set_coherent_mask(&pdev->dev,
 				DMA_BIT_MASK(DHD_PCIE_DMA_MASK_FOR_GS101))) {
 			DHD_ERROR(("%s: DMA set %d bit mask failed.\n",
 				__FUNCTION__, DHD_PCIE_DMA_MASK_FOR_GS101));

linux_osl.c
@@ -1130,6 +1130,7 @@ osl_dma_alloc_consistent(osl_t *osh, uint size, uint16 align_bits, uint *alloced
 void
 osl_dma_free_consistent(osl_t *osh, void *va, uint size, dmaaddr_t pa)
 {
+	struct pci_dev *pdev = osh->pdev;
 #ifdef BCMDMA64OSL
 	dma_addr_t paddr;
 #endif /* BCMDMA64OSL */
@@ -1140,9 +1141,9 @@ osl_dma_free_consistent(osl_t *osh, void *va, uint size, dmaaddr_t pa)
 #else
 #ifdef BCMDMA64OSL
 	PHYSADDRTOULONG(pa, paddr);
-	pci_free_consistent(osh->pdev, size, va, paddr);
+	dma_free_coherent(&pdev->dev, size, va, paddr);
 #else
-	pci_free_consistent(osh->pdev, size, va, (dma_addr_t)pa);
+	dma_free_coherent(&pdev->dev, size, va, (dma_addr_t)pa);
 #endif /* BCMDMA64OSL */
 #endif /* __ARM_ARCH_7A__ && !DHD_USE_COHERENT_MEM_FOR_RING */
 }
@@ -1165,6 +1166,7 @@ dmaaddr_t
 BCMFASTPATH(osl_dma_map)(osl_t *osh, void *va, uint size, int direction, void *p,
 	hnddma_seg_map_t *dmah)
 {
+	struct pci_dev *pdev = osh->pdev;
 	int dir;
 	dmaaddr_t ret_addr;
 	dma_addr_t map_addr;
@@ -1177,9 +1179,9 @@ BCMFASTPATH(osl_dma_map)(osl_t *osh, void *va, uint size, int direction, void *p
 	/* For Rx buffers, keep direction as bidirectional to handle packet fetch cases */
 	dir = (direction == DMA_RX)? DMA_RXTX: direction;
 
-	map_addr = pci_map_single(osh->pdev, va, size, dir);
+	map_addr = dma_map_single(&pdev->dev, va, size, dir);
 
-	ret = pci_dma_mapping_error(osh->pdev, map_addr);
+	ret = dma_mapping_error(&pdev->dev, map_addr);
 
 	if (ret) {
 		OSL_PRINT(("%s: Failed to map memory\n", __FUNCTION__));
@@ -1202,6 +1204,7 @@ BCMFASTPATH(osl_dma_map)(osl_t *osh, void *va, uint size, int direction, void *p
 void
 BCMFASTPATH(osl_dma_unmap)(osl_t *osh, dmaaddr_t pa, uint size, int direction)
 {
+	struct pci_dev *pdev = osh->pdev;
 	int dir;
 #ifdef BCMDMA64OSL
 	dma_addr_t paddr;
@@ -1220,9 +1223,9 @@ BCMFASTPATH(osl_dma_unmap)(osl_t *osh, dmaaddr_t pa, uint size, int direction)
 
 #ifdef BCMDMA64OSL
 	PHYSADDRTOULONG(pa, paddr);
-	pci_unmap_single(osh->pdev, paddr, size, dir);
+	dma_unmap_single(&pdev->dev, paddr, size, dir);
 #else /* BCMDMA64OSL */
-	pci_unmap_single(osh->pdev, (uint32)pa, size, dir);
+	dma_unmap_single(&pdev->dev, (uint32)pa, size, dir);
 #endif /* BCMDMA64OSL */
 
 	DMA_UNLOCK(osh);

FundamentalOS/android_kernel_google_gs-6.1_google-modules

(cherry picked from commit 1e1694d6a7d8fcbf1b15f57fe070b9bf4b0ede21)
Signed-off-by: Jacky Liu <qsliu@google.com>

dhd_pcie_linux.c
@@ -340,8 +340,8 @@ static int dhdpcie_smmu_init(struct pci_dev *pdev, void *smmu_cxt)
 
 	DHD_ERROR(("%s : SMMU init start\n", __FUNCTION__));
 
-	if (pci_set_dma_mask(pdev, DMA_BIT_MASK(64)) ||
-		pci_set_consistent_dma_mask(pdev, DMA_BIT_MASK(64))) {
+	if (dma_set_mask(&pdev->dev, DMA_BIT_MASK(64)) ||
+		dma_set_coherent_mask(&pdev->dev, DMA_BIT_MASK(64))) {
 		DHD_ERROR(("%s: DMA set 64bit mask failed.\n", __FUNCTION__));
 		return -EINVAL;
 	}
@@ -2098,8 +2098,8 @@ int dhdpcie_init(struct pci_dev *pdev)
 
 #ifdef DHD_CUSTOMER_PCIE_DMA_MASK
 		/* S.SLSI PCIe DMA engine cannot support 64 bit bus address. Set specified bit */
-		if (pci_set_dma_mask(pdev, DMA_BIT_MASK(DHD_CUSTOMER_PCIE_DMA_MASK)) ||
-			pci_set_consistent_dma_mask(pdev,
+		if (dma_set_mask(&pdev->dev, DMA_BIT_MASK(DHD_CUSTOMER_PCIE_DMA_MASK)) ||
+			dma_set_coherent_mask(&pdev->dev,
 				DMA_BIT_MASK(DHD_CUSTOMER_PCIE_DMA_MASK))) {
 			DHD_ERROR(("%s: DMA set %d bit mask failed.\n",
 				__FUNCTION__, DHD_CUSTOMER_PCIE_DMA_MASK));

linux_osl.c
@@ -1149,6 +1149,7 @@ osl_dma_alloc_consistent(osl_t *osh, uint size, uint16 align_bits, uint *alloced
 void
 osl_dma_free_consistent(osl_t *osh, void *va, uint size, dmaaddr_t pa)
 {
+	struct pci_dev *pdev = osh->pdev;
 #ifdef BCMDMA64OSL
 	dma_addr_t paddr;
 #endif /* BCMDMA64OSL */
@@ -1159,9 +1160,9 @@ osl_dma_free_consistent(osl_t *osh, void *va, uint size, dmaaddr_t pa)
 #else
 #ifdef BCMDMA64OSL
 	PHYSADDRTOULONG(pa, paddr);
-	pci_free_consistent(osh->pdev, size, va, paddr);
+	dma_free_coherent(&pdev->dev, size, va, paddr);
 #else
-	pci_free_consistent(osh->pdev, size, va, (dma_addr_t)pa);
+	dma_free_coherent(&pdev->dev, size, va, (dma_addr_t)pa);
 #endif /* BCMDMA64OSL */
 #endif /* __ARM_ARCH_7A__ && !DHD_USE_COHERENT_MEM_FOR_RING */
 }
@@ -1184,6 +1185,7 @@ dmaaddr_t
 BCMFASTPATH(osl_dma_map)(osl_t *osh, void *va, uint size, int direction, void *p,
 	hnddma_seg_map_t *dmah)
 {
+	struct pci_dev *pdev = osh->pdev;
 	dmaaddr_t ret_addr;
 	dma_addr_t map_addr;
 	int ret;
@@ -1192,9 +1194,9 @@ BCMFASTPATH(osl_dma_map)(osl_t *osh, void *va, uint size, int direction, void *p
 
 	ASSERT((osh && (osh->magic == OS_HANDLE_MAGIC)));
 
-	map_addr = pci_map_single(osh->pdev, va, size, direction);
+	map_addr = dma_map_single(&pdev->dev, va, size, direction);
 
-	ret = pci_dma_mapping_error(osh->pdev, map_addr);
+	ret = dma_mapping_error(&pdev->dev, map_addr);
 
 	if (ret) {
 		printk("%s: Failed to map memory\n", __FUNCTION__);
@@ -1217,6 +1219,7 @@ BCMFASTPATH(osl_dma_map)(osl_t *osh, void *va, uint size, int direction, void *p
 void
 BCMFASTPATH(osl_dma_unmap)(osl_t *osh, dmaaddr_t pa, uint size, int direction)
 {
+	struct pci_dev *pdev = osh->pdev;
 #ifdef BCMDMA64OSL
 	dma_addr_t paddr;
 #endif /* BCMDMA64OSL */
@@ -1231,9 +1234,9 @@ BCMFASTPATH(osl_dma_unmap)(osl_t *osh, dmaaddr_t pa, uint size, int direction)
 
 #ifdef BCMDMA64OSL
 	PHYSADDRTOULONG(pa, paddr);
-	pci_unmap_single(osh->pdev, paddr, size, direction);
+	dma_unmap_single(&pdev->dev, paddr, size, direction);
 #else /* BCMDMA64OSL */
-	pci_unmap_single(osh->pdev, (uint32)pa, size, direction);
+	dma_unmap_single(&pdev->dev, (uint32)pa, size, direction);
 #endif /* BCMDMA64OSL */
 
 	DMA_UNLOCK(osh);