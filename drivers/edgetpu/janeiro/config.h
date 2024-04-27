a1f40e0455e54a69f3f7e8fb5b4945ff69be54a0
Whi copybara merger <whitechapel-automerger@google.com>
2024-04-27 03:07:43 +0000

    edgetpu: remove debug dump feature
    Remove unused debug dump feature (and reclaim associated coherent
    memory).
    
    Bug: 337348834
    GitOrigin-RevId: 87f253ce3993d4158ef694fb38cb9c34acf19754
    Change-Id: I67748b4d4e8876755553bc5cbf82343cf7544284
    Signed-off-by: Todd Poynor <toddpoynor@google.com>

--- drivers/edgetpu/janeiro/config.h
+++ drivers/edgetpu/janeiro/config.h
@@ -75,14 +75,6 @@
 #define EDGETPU_REMAPPED_DATA_ADDR                                        \
 	(EDGETPU_INSTRUCTION_REMAP_BASE + EDGETPU_REMAPPED_DATA_OFFSET)
 
-/*
- * Size is determined by calculating total size of structs in
- * edgetpu_debug_dump.h and the size of FW side memory segments from linker.ld
- * in the FW source code. Some extra head room is provided for segments that
- * are not fixed length such as crash reason and debug stats.
- */
-#define EDGETPU_DEBUG_DUMP_MEM_SIZE 0x321000
-
 #include "config-mailbox.h"
 #include "config-pwr-state.h"
 #include "config-tpu-cpu.h"
