4ba860bca074a892e9aef24292f12b61a335d9f1
attis <attis@google.com>
2024-08-05 18:56:38 +0800

    panel: tg4a: tune underrun_param
    Change the tg4a te_idle_us to 1000.
    
    Test: build and check no underrun log after suspend/resume.
    Bug: 354806117
    Change-Id: Ibf407898e1b3b23f0b02779b041daad9de3e8fd6
    Signed-off-by: attis <attis@google.com>

--- panel-gs-tg4a.c
+++ panel-gs-tg4a.c
@@ -1031,7 +1031,7 @@ static void tg4a_set_ssc_en(struct gs_panel *ctx, bool enabled)
 }
 
 static const struct gs_display_underrun_param underrun_param = {
-	.te_idle_us = 2510,
+	.te_idle_us = 1000,
 	.te_var = 1,
 };
 
